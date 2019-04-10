26228b70a134c82d789001a29f76d8955a7291dc
kishangz <Kishanz@Kishans-MacBook-Pro.local>
2019-04-09 22:02:42 -0400

    Created Signup Page and Photocontroller

---
+++ src/model/Tag.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Tag {
+	
+	private String type, value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public String getTag() {
+		// TODO Auto-generated method stub
+		return type;
+	}
+
+}
